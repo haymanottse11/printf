84d2918871251e90f739f3b4567f03f7d1e89242
haymanottse11 <haymanottse11@gmail.com>
2022-07-12 15:32:08 -0700

    ROT13

--- print_rot.c
+++ print_rot.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print_rot - writes the str in ROT13
- * @arguments: input string
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
+ * print_rot - writes the str in ROT13.
+ * @arguments: input string.
+ * @buf: buffer pointer.
+ * @ibuf: index for buffer pointer.
  *
  * Return: number of chars printed.
  */
